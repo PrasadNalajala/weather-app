90dadaeede5c0c371048321ab08b85ceb56ea0c2
Prasad <nalajalaprasad1018@gmail.com>
2024-02-01 17:12:45 +0530

    updated

--- src/FiveDayForecast/index.js
+++ src/FiveDayForecast/index.js
@@ -1,6 +1,42 @@
 import './index.css'
 
-const FiveDayForecast=()=>{
+const FiveDayForecast=(props)=>{
+    const {FiveDayForeCast,weather}=props
+    let formattedForecast=[]
+if (weather.list) {
+    
+    const weatherList = weather.list;
+  
+   
+    const uniqueForecast = {};
+  
+   
+    weatherList.forEach(entry => {
+      const entryDate = new Date(entry.dt_txt).toLocaleDateString();
+  
+      
+      if (!uniqueForecast[entryDate]) {
+        uniqueForecast[entryDate] = {
+          day: new Date(entry.dt_txt).toLocaleDateString('en-US', { weekday: 'long' }),
+          chanceOfRain: entry.pop * 100, 
+          humidity: entry.main.humidity,
+          wind: {
+            speed: entry.wind.speed,
+            direction: entry.wind.deg, 
+          },
+          temperature: entry.main.temp,
+        };
+      }
+    });
+  
+     formattedForecast = Object.values(uniqueForecast);
+  
+    //console.log(formattedForecast);
+  } else {
+    console.error("Invalid data format. 'list' property not found.");
+  }
+  
+
     return(
         <div className='card-4'>
             <div className='card-details'>
@@ -14,16 +50,32 @@ const FiveDayForecast=()=>{
               
             </div>
             <hr/>
-            <div className='card-details'>
-                <div className='forecast-container'>
-                    <p className='forecast-p day day-p'>Monday</p>
-                    <p className='forecast-p'>9%</p>
-                    <p className='forecast-p'>57%</p>
-                    <p className='forecast-p'>10km/h</p>
-                    <p className='forecast-p'>19</p>
-                </div>
-              
-            </div>
+
+            {formattedForecast.length !== 0 ? (
+  formattedForecast.map((each, index) => (
+    <div key={index} className='card-details'>
+      <div className='forecast-container'>
+        <p className='forecast-p day day-p'>{each.day}</p>
+        <p className='forecast-p'>{each.chanceOfRain}%</p>
+        <p className='forecast-p'>{each.humidity}%</p>
+        <p className='forecast-p'>{each.wind.speed}km/h</p>
+        <p className='forecast-p'>{each.temperature}</p>
+      </div>
+    </div>
+  ))
+) : (
+  <div className='card-details'>
+    <div className='forecast-container'>
+      <p className='forecast-p day day-p'>Monday</p>
+      <p className='forecast-p'>9%</p>
+      <p className='forecast-p'>57%</p>
+      <p className='forecast-p'>10km/h</p>
+      <p className='forecast-p'>19</p>
+    </div>
+  </div>
+)}
+
+            
             <hr/>
         </div>
     )
--- src/Now/index.js
+++ src/Now/index.js
@@ -1,6 +1,15 @@
 import './index.css'
 
-const Now=()=>{
+const Now=(props)=>{
+    const {weather}=props
+    const firstWeatherItem = weather.list[0];
+
+// Extracting relevant fields
+const chanceOfRain = firstWeatherItem.pop; // Probability of precipitation
+const humidity = firstWeatherItem.main.humidity;
+const windSpeed = firstWeatherItem.wind.speed;
+const visibility = firstWeatherItem.visibility;
+const pressure = firstWeatherItem.main.pressure;
     return(
         <div className='card-3'>
             
@@ -8,23 +17,23 @@ const Now=()=>{
             <div className='day-details-container'>
             <div className='day-details1'>
                 <p className='measure'>Chance of Rain</p>
-                <h1 className='value'>12%</h1>
+                <h1 className='value'>{chanceOfRain}%</h1>
             </div>
             <div className='day-details1'>
                 <p className='measure'>Humidity</p>
-                <h1 className='value'>60%</h1>
+                <h1 className='value'>{humidity}%</h1>
             </div>
             <div className='day-details1'>
                 <p className='measure'>Wind Speed</p>
-                <h1 className='value'>10 k/ph</h1>
+                <h1 className='value'>{windSpeed}k/ph</h1>
             </div>
             <div className='day-details1'>
                 <p className='measure'>Visibility</p>
-                <h1 className='value'>1000km</h1>
+                <h1 className='value'>{visibility}km</h1>
             </div>
             <div className='day-details1'>
                 <p className='measure'>Pressure</p>
-                <h1 className='value'>1015 hPa</h1>
+                <h1 className='value'>{pressure} hPa</h1>
             </div>
             </div>
         </div>
